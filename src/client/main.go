47e52991a0b30efb515a3dbf45b750ef89035c11
Ramax Lo <ramaxlo@gmail.com>
2016-11-14 22:29:21 +0800

    client: Calculate failure count
    When encountering read/write failure, increment the failure count by 1.

--- src/client/main.go
+++ src/client/main.go
@@ -7,8 +7,15 @@ import (
 	"os"
 	"strconv"
 	"sync"
+	"sync/atomic"
 	"time"
 )
+
+var fail_count int32
+
+func fail() {
+	atomic.AddInt32(&fail_count, 1)
+}
 
 func usage() {
 	fmt.Println("Usage: ./client <addr> <num of connections>")
@@ -16,6 +23,11 @@ func usage() {
 
 func start(l *log.Logger, idx int, wg *sync.WaitGroup, addr string) {
 	defer wg.Done()
+	defer func() {
+		if r := recover(); r != nil {
+			fail()
+		}
+	}()
 
 	conn, err := net.Dial("tcp", addr)
 	if err != nil {
@@ -29,8 +41,17 @@ func start(l *log.Logger, idx int, wg *sync.WaitGroup, addr string) {
 		s := fmt.Sprintf("client %d send %d", idx, i)
 		l.Printf("%s\n", s)
 
-		conn.Write([]byte(s))
-		n, _ := conn.Read(buf)
+		n, err := conn.Write([]byte(s))
+		if err != nil {
+			l.Printf("client %d fail to write: %s\n", idx, err)
+			panic("write fail")
+		}
+
+		n, err = conn.Read(buf)
+		if err != nil {
+			l.Printf("client %d fail to read: %s\n", idx, err)
+			panic("read fail")
+		}
 
 		l.Printf("%s\n", string(buf[:n]))
 		time.Sleep(1 * time.Second)
@@ -49,9 +70,11 @@ func main() {
 	num, _ := strconv.Atoi(os.Args[2])
 	wg := &sync.WaitGroup{}
 	for i := 0; i < num; i++ {
+		time.Sleep(1 * time.Millisecond)
 		go start(l, i, wg, addr)
+		wg.Add(1)
 	}
 
-	wg.Add(num)
 	wg.Wait()
+	l.Printf("Fail count: %d\n", fail_count)
 }
